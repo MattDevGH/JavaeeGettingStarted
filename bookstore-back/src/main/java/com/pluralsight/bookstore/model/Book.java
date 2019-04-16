d7b1ed0abc2e3e2d70360e0691d42982b28db55a
MattDevGH <38076916+MattDevGH@users.noreply.github.com>
2019-04-16 13:56:25 +0100

    added project files so far

---
+++ bookstore-back/src/main/java/com/pluralsight/bookstore/model/Book.java
@@ -0,0 +1,119 @@
+package com.pluralsight.bookstore.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Book {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(length = 200)
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column(name = "unit_cose")
+    private Float unitCost;
+
+    private String isbn;
+
+    @Column(name = "publication_date")
+    @Temporal(TemporalType.DATE)
+    private Date publicationDate;
+
+    private Integer numPages;
+
+    private String coverImageUrl;
+
+    private Language language;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(Float unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Integer getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(Integer numPages) {
+        this.numPages = numPages;
+    }
+
+    public String getCoverImageUrl() {
+        return coverImageUrl;
+    }
+
+    public void setCoverImageUrl(String coverImageUrl) {
+        this.coverImageUrl = coverImageUrl;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", unitCost=" + unitCost +
+                ", isbn='" + isbn + '\'' +
+                ", publicationDate=" + publicationDate +
+                ", numPages=" + numPages +
+                ", coverImageUrl='" + coverImageUrl + '\'' +
+                ", language=" + language +
+                '}';
+    }
+}
